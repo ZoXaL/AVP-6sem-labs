63a8cd67b0ef602bf7a8113a89fbf854860b7bec
zoxal <zzoxall@gmail.com>
2018-02-09 13:21:34 +0300

    lab3 passed

--- lab3/main.c
+++ lab3/main.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <time.h>
-#include <assert.h>
 #include <stdlib.h>
 #include <sys/types.h>
 
@@ -14,7 +11,6 @@
 #define BYTES_IN_MB				1024 * 1024
 #define MAX_CACHE_WAY			20
 #define CACHE_SIZE_KB			64
-#define DELIMETER_SIZE_BYTES	BYTES_IN_MB - STR_SIZE_BYTES
 #define CELL_SIZE_BYTES			64
 #define CELL_PER_STR			(CACHE_SIZE_KB * BYTES_IN_KB) / (MAX_CACHE_WAY * CELL_SIZE_BYTES)
 #define STR_SIZE_BYTES 			CELL_PER_STR * CELL_SIZE_BYTES
@@ -30,6 +26,7 @@ struct unit *mass;
 struct unit* prepare_mass_to_way(int way)
 {
 	struct unit *mass = (struct unit*) malloc(MAX_CACHE_WAY * BYTES_IN_MB);
+
 	for (int i = 0; i < CELL_PER_STR; i++) {
 		for (int j = 0; j < way; j++) {
 			struct unit* next;
@@ -61,28 +58,36 @@ double measure_way_time(struct unit* mass, int way)
 	struct unit *tmp = mass;
 	unsigned long long start_time = rdtsc();
 	for (int k = 0; k < N; k++) {
-		tmp = tmp->next;
+		// printf("%ld\n", tmp);
+		tmp = tmp->next;		
 	}
 	unsigned long long end_time = rdtsc();
 	unsigned long long result = end_time - start_time;
-	return (double)(result / N);
+	return (double)((double)result / N);
 }
 
 int main(int argc, char *argv[]) {
 	struct unit* mass;
 	double x[MAX_CACHE_WAY];
 	double y[MAX_CACHE_WAY];
-	for (int i = 1; i < MAX_CACHE_WAY; i++) {
+
+	if (strcmp("plot", argv[1]) == 0) {
+		for (int i = 1; i < MAX_CACHE_WAY; i++) {
 			x[i] = (double)i;
 			mass = prepare_mass_to_way(i);
 			y[i] = measure_way_time(mass, i);
 			printf("%d-%F\n", i, y[i]);
 			free(mass);
+		}
+		gnuplot_ctrl *h = gnuplot_init();
+		gnuplot_setstyle(h, "linespoints");
+		gnuplot_plot_xy(h, x, y, MAX_CACHE_WAY, "avg time clocks");
+		getchar();
+		gnuplot_close(h);
+	} else {
+		int way = atoi(argv[1]);
+		mass = prepare_mass_to_way(way);
+		printf("%F\n", measure_way_time(mass, way));		
 	}
-	gnuplot_ctrl *h = gnuplot_init();
-	gnuplot_setstyle(h, "linespoints");
-	gnuplot_plot_xy(h, x, y, MAX_CACHE_WAY, "avg time clocks");
-	getchar();
-	gnuplot_close(h);
 	return 0;
 }
